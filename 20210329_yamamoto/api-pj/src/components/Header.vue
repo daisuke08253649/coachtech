03aac57199be227f959c9f2db64faf334851e004
daisuke14012719 <79070611+daisuke14012719@users.noreply.github.com>
2021-03-21 12:03:41 +0900

    Add files via upload

---
+++ 20210329_yamamoto/api-pj/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="Header">
+        <input type="text" v-model="fisPostCode">
+        <button @click="address">住所自動入力</button>
+        <p>Address:{{address}}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            text:""
+        };
+    },
+    methods:{
+        address(){
+            this.$emit("gettext",this.text);
+        }
+    }
+};
+</script>
