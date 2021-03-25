d3fad19e4a2721d879c4028acc6d5fb1e6561090
daisuke14012719 <79070611+daisuke14012719@users.noreply.github.com>
2021-03-26 08:36:54 +0900

    Add files via upload

--- 20210329_yamamoto/api-pj/src/views/Home.vue
+++ 20210329_yamamoto/api-pj/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <input type="text" v-model="postcode" maxlength="7" />
+    <button @click="searchAddress">住所自動入力</button>
+    <p>Address:{{addressdata}}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data(){
+    return{
+      addressdata:"",
+      apikey:"fe99bc357b10c34acd98782807a0a2cf",
+    };
+  },
+  methods:{
+    searchaddress() {
+      let url= "https://apis.postcode-jp.com/api/v3/postcodes/"
+      this.postcode 
+      "apikey=" 
+      this.apikey;
+      axios.get(url).then((res) => {
+        this.addressdata = res.data.alladdress;
+      });
+    },
+  },
+};
 </script>
