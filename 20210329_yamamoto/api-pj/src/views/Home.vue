83d52b62871d89b8178a99f79fd0811d907b01f1
daisuke14012719 <79070611+daisuke14012719@users.noreply.github.com>
2021-03-29 09:52:40 +0900

    Add files via upload

--- 20210329_yamamoto/api-pj/src/views/Home.vue
+++ 20210329_yamamoto/api-pj/src/views/Home.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="home">
-    <input type="text" v-model="postcode" maxlength="7" />
-    <button @click="searchAddress">住所自動入力</button>
-    <p>Address:{{addressdata}}</p>
+  <div class="home">
+    <input type="text" v-model="zipcode" maxlength="7" />
+    <button @click="searchAddressInfo">住所自動入力</button>
+    <p>Address：{{ addressData }}</p>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  data(){
-    return{
-      addressdata:"",
-      apikey:"fe99bc357b10c34acd98782807a0a2cf",
+  data() {
+    return {
+      zipcode: null,
+      addressData: "",
+      apiKey: "4Mo1DfviFePweEWrRr6wDLqGZbPTrY2ZTr8OFq2",
     };
   },
-  methods:{
-    searchaddress() {
-      let url= "https://apis.postcode-jp.com/api/v3/postcodes/"
-      this.postcode 
-      "apikey=" 
-      this.apikey;
+  methods: {
+    searchAddressInfo() {
+      let url =
+        "https://apis.postcode-jp.com/api/v3/postcodes/" +
+        this.zipcode +
+        "?apiKey=" +
+        this.apiKey;
       axios.get(url).then((res) => {
-        this.addressdata = res.data.alladdress;
+        this.addressData = res.data.allAddress;
       });
     },
   },
